7fff98600df9ffa3ab7e245f96f4b9e0ed3dbc58
solomonmulatu <mulatusolomon@gmail.com>
2020-09-13 13:10:01 -0700

    Updates

---
+++ precache-manifest.f90b94481d750bb7a89ab5df3aa9443e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094d734471c81b521e868003a601fcbc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23c7769cdab9689c36be",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23c7769cdab9689c36be",
+    "url": "/static/js/main.1fa4da54.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
